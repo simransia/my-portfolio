344e02b1332c993a358e0fac122120912f24d56a
Simran Chaurasia <simransia07@gmail.com>
2024-08-20 11:17:06 +0530

    revert import changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import ButterFly from "@/models/butterfly";
 import BlueButterFly from "@/models/butterFlyBlue";
 import ShiningButterFly from "@/models/butterflyShining";
 import CampScene from "@/models/Camp/";
-import Ring from "../models/Ring";
+import Ring from "@/models/Ring";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
