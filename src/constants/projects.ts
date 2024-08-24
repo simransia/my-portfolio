b15739e176ea2ba4ca2142fe4a91b8e1ac4d9ac9
Simran Chaurasia <simransia07@gmail.com>
2024-08-24 11:04:33 +0530

    optimizing app

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -6,7 +6,8 @@ export const PERSONAL_PROJECTS = [
     desc: "Disney Clone built using ReactJs and Firebase and TMDB APIs. It also has AI search functionality using which you can search movies based on or mood by typing any kind of query in searchbar",
     img: "",
     link: "https://disney-clone-theta-flax.vercel.app/",
-    video: "/videos/disney-clone.mp4",
+    video:
+      "https://res.cloudinary.com/dbstfw8yc/video/upload/v1724477325/simmi-portfolio/disney-clone_idmhct.mp4",
     techStacks: [
       "React js",
       "Firebase",
@@ -22,7 +23,8 @@ export const PERSONAL_PROJECTS = [
     desc: "iNotebook is a full-stack application built using the MERN stack. It helps you to store your notes securely on the cloud. It also gives you the functionality to edit and delete your previous written notes.",
     img: "",
     link: "https://notebook-onthecloud.vercel.app/",
-    video: "/videos/inotebook.mp4",
+    video:
+      "https://res.cloudinary.com/dbstfw8yc/video/upload/v1724477400/simmi-portfolio/inotebook_uolud8.mp4",
     techStacks: [
       "React js",
       "Node js",
@@ -40,7 +42,8 @@ export const PERSONAL_PROJECTS = [
     desc: "This app can be used for meditation and unwinding. It has 4 different themes containing different soothing sounds of nature like the sound of birds, the sound of rain, and the sound of sea waves, which are combined with the calming music of piano & flute.",
     img: "",
     link: "https://simransia.github.io/Musing/",
-    video: "/videos/musing.mp4",
+    video:
+      "https://res.cloudinary.com/dbstfw8yc/video/upload/v1724477410/simmi-portfolio/musing_bach06.mp4",
     techStacks: ["HTML", "CSS", "Javascript"],
   },
   {
@@ -50,7 +53,8 @@ export const PERSONAL_PROJECTS = [
     desc: "Click on the images to see the beautiful animations :) along with inspiraing quotes to make your day",
     img: "",
     link: "https://simransia.github.io/image-gallery/",
-    video: "/videos/image-gallary.mp4",
+    video:
+      "https://res.cloudinary.com/dbstfw8yc/video/upload/v1724477405/simmi-portfolio/image-gallary_gntkgf.mp4",
     techStacks: ["HTML", "CSS"],
   },
 ];
